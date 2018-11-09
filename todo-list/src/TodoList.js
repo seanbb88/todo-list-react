950342bb978e21c73c0258762a59e7ce8d818364
Sean Brown <sbbrown35@gmail.com>
2018-11-09 10:03:34 -0600

    third

--- todo-list/src/TodoList.js
+++ todo-list/src/TodoList.js
@@ -16,17 +16,19 @@ class TodoList extends Component {
 
     componentDidMount(){
         /* Create reference to messages in Firebase Database */
-        let messagesRef = fire.database().ref('messages').orderByKey().limitToLast(100);
-        messagesRef.on('child_added', snapshot => {
+        let itemsRef = fire.database().ref('items').orderByKey().limitToLast(100);
+        itemsRef.on('child_added', snapshot => {
           /* Update React state when message is added at Firebase Database */
-          let message = { text: snapshot.val(), id: snapshot.key };
-          this.setState({ messages: [message].concat(this.state.messages) });
+          let items = { text: snapshot.val(), id: snapshot.key };
+          this.setState({ items: [items].concat(this.state.items) });
         })
       }
 
     addItem(e){
         e.preventDefault(); 
         if(this._inputElement.value !== ""){
+            fire.database().ref('items').push(this._inputElement.value)
+
             var newItem = {
                 text: this._inputElement.value,
                 key: Date.now(),
